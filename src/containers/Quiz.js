02635ced037df1ee92a15d22a5a52596aa728db4
GodwinEgo <godwinehile04@gmail.com>
2023-07-19 14:33:45 +0100

    redux

--- src/containers/Quiz.js
+++ src/containers/Quiz.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Question from "../components/Question";
+import { useSelector } from "react-redux";
 
 const Quiz = () => {
-  return <div>Quiz</div>;
+  const [currenQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState("");
+  const quizzes = useSelector((state) => state.quiz.quizzes);
+
+  const handleNextQuestion = () => {
+    //logic for next question
+  };
+
+  const handleSelectedOption = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div>
+      {quizzes.length > 0 && currenQuestion < quizzes.length && (
+        <Question
+          question={quizzes[currenQuestion].questions[currenQuestion].question}
+          options={quizzes[currentQuestion].questions[currentQuestion].options}
+          selectedOption={selectedOption}
+          onSelectOption={handleSelectOption}
+        />
+      )}
+      {/* BUTTON FOR HANDLING NEXT QUESTION */}
+    </div>
+  );
 };
 
 export default Quiz;
