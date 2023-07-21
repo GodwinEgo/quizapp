ebbb50da76839d2a2c33324ffde4b43d905a3362
GodwinEgo <godwinehile04@gmail.com>
2023-07-21 20:16:31 +0100

    style

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,15 +1,27 @@
 // src/components/LandingPage.js
 import React from "react";
+import { Button, Container, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
-    <div className="landing-page">
-      <h1>Welcome to the Quiz App</h1>
-      <Link to="/quiz" className="start-quiz-button">
+    <Container maxWidth="sm" sx={{ marginTop: "30vh", textAlign: "center" }}>
+      <Typography variant="h4" gutterBottom>
+        Welcome to the Quiz App!
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        Test your knowledge with various quizzes.
+      </Typography>
+      <Button
+        component={Link}
+        to="/quiz"
+        variant="contained"
+        color="primary"
+        sx={{ marginTop: 2 }}
+      >
         Start Quiz
-      </Link>
-    </div>
+      </Button>
+    </Container>
   );
 };
 
