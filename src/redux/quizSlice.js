dc61645be7d2e28a9c943d6291910427acaa7c3f
GodwinEgo <godwinehile04@gmail.com>
2023-07-21 16:13:17 +0100

    quiz

--- src/redux/quizSlice.js
+++ src/redux/quizSlice.js
@@ -2,7 +2,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import quizzes from "../data/quizzes";
 
-// Function to shuffle an array using the Fisher-Yates algorithm
 const shuffleArray = (array) => {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
@@ -11,7 +10,6 @@ const shuffleArray = (array) => {
   return array;
 };
 
-// Make sure each quiz object has a questions property (empty array if no questions)
 const quizzesWithQuestions = quizzes.map((quiz) => ({
   ...quiz,
   questions: quiz.questions || [],
@@ -37,7 +35,7 @@ const quizSlice = createSlice({
           ...question,
           selectedOption: "",
           isAnswered: false,
-        })); // Shuffle and select the first 20 questions with initial state
+        }));
       state.currentQuestion = 0;
       state.score = 0;
       state.isQuizCompleted = false;
@@ -48,50 +46,42 @@ const quizSlice = createSlice({
     },
     nextQuestion(state) {
       const currentQuestionData = state.quizzes[state.currentQuestion];
-
-      // Mark the question as answered
       currentQuestionData.isAnswered = true;
-
       if (currentQuestionData.answer === currentQuestionData.selectedOption) {
-        // Increase score if the selected option is correct
         state.score += 1;
       }
-
-      // Move to the next question
       if (state.currentQuestion < state.quizzes.length - 1) {
         state.currentQuestion += 1;
       }
     },
     prevQuestion(state) {
-      // Move to the previous question
       if (state.currentQuestion > 0) {
         state.currentQuestion -= 1;
       }
     },
     submitQuiz(state) {
-      // Calculate the score based on the selected options
       state.score = state.quizzes.reduce((acc, question) => {
         if (question.answer === question.selectedOption) {
           return acc + 1;
         }
         return acc;
       }, 0);
-
-      // Mark all questions as answered
       state.quizzes.forEach((question) => {
         question.isAnswered = true;
       });
-
-      // Reset the current question index
       state.currentQuestion = 0;
       state.isQuizCompleted = true;
     },
     resetQuiz(state) {
-      // Reset the quiz state
-      state.quizzes.forEach((question) => {
-        question.selectedOption = "";
-        question.isAnswered = false;
-      });
+      state.quizzes = shuffleArray(
+        quizzesWithQuestions.flatMap((quiz) => quiz.questions)
+      )
+        .slice(0, 20)
+        .map((question) => ({
+          ...question,
+          selectedOption: "",
+          isAnswered: false,
+        }));
       state.currentQuestion = 0;
       state.score = 0;
       state.isQuizCompleted = false;
